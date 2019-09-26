fed86e7dc223a341bcb4bbd32d280d5e60f1d351
Vi Pham <Vi.Pham@inspection.gc.ca>
2019-09-26 16:32:30 -0400

    processing

--- exercise3.js
+++ exercise3.js
@@ -1,23 +1,57 @@
 const fs = require("fs");
 const readline = require("readline");
 
-let path =
-  "C:\\Users\\vipha\\Desktop\\study\\Computer 4\\Language\\Exercise 3\\CST8333 19F Dataset Loader\\CST8333 19F Dataset Loader\\canadianCheeseDirectory.csv";
+let path = "javascript_learning\\canadianCheeseDirectory.csv";
 let max = 5;
 let counter = 0;
+//create an array of rows
+let totalRows = [];
 
-//open the file
-//read the file
-//because I dont want to read the whole file, read stream
-//use readline
+//create an array of rows with selected columns
+let rows = [];
+//create header(column name)
+let header = [];
+
+//create an array of data without header
+let data = [];
 
 let readFile = fs.createReadStream(path);
 let rl = readline.createInterface({ input: readFile });
-rl.on("line", data => {
-  console.log(data);
+rl.on("line", d => {
+  //put the line into an array
+  //console.log(data.split(","));
+  totalRows.push(d.split(","));
+  rows = [
+    totalRows[0],
+    totalRows[1],
+    totalRows[3],
+    totalRows[5],
+    totalRows[6],
+    totalRows[8]
+  ];
+  //put column names into header
+  header = rows[0];
+  data = rows.slice(1);
   if (counter++ >= max) {
     //stop reading
     rl.close();
     rl.removeAllListeners();
+    console.log(rows);
+    console.log(header);
   }
 });
+
+// var Converter = require("csvtojson").Converter;
+// var converter = new Converter({});
+// converter.fromFile("javascript_learning\\canadianCheeseDirectory.csv",function(err,result){
+//   var csvData = JSON.stringify
+//   ([
+//     {resultdata : result[0]},
+//     {resultdata : result[1]},
+//     {resultdata : result[2]},
+//     {resultdata : result[3]},
+//     {resultdata : result[4]}
+//   ]);
+//   csvData = JSON.parse(csvData);
+//   console.log(csvData);
+// });
